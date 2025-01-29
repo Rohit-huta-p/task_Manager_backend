20da4992ae532603980a0c57b6fa293bf1cfc9a4
Apple <apple@Apples-MacBook-Air.local>
2025-01-29 13:05:43 +0530

    cors url update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors( 
     {
-        origin: 'https://swissnote-ass-frontend.vercel.app',
+        origin: 'https://task-manager-ass.vercel.app/',
         credentials: true,
     }
         ));
